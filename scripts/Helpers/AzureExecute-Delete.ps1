04608ef13f06f453aaf2e45234e04c23b293284d
Colin Vermander <colin.vermander@adoxio.com>
2019-01-22 12:26:04 -0500

    Update to uncomment execute command

--- scripts/Helpers/AzureExecute-Delete.ps1
+++ scripts/Helpers/AzureExecute-Delete.ps1
@@ -11,17 +11,12 @@
     [string]$Password
 )
 
-Write-Verbose "Username: $UserName"
-Write-Verbose "Password: $Password"
-
 $connParams = @{
     OrganizationName = $UniqueName 
     ServerUrl = "https://$OrgName.$Region.dynamics.com"
     Credential = [PSCredential]::new($UserName, ($Password | ConvertTo-SecureString -AsPlainText -Force))
 }
 
-$connParams
+Write-Verbose $connParams
 
-$connParams.Credential
-
-#.\Helpers\Execute-DeleteDataFromJson.ps1 -CrmConnectionParameters $connParams -Verbose -RecordDirectory /deletes/
+.\Execute-DeleteDataFromJson.ps1 -CrmConnectionParameters $connParams -Verbose -RecordDirectory /deletes/
