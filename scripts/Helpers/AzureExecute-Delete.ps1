99f3e76cf72df10c379319ccfe2dc38665778f1a
Colin Vermander <colin.vermander@adoxio.com>
2019-01-22 12:12:18 -0500

    Add azure delete script

---
+++ scripts/Helpers/AzureExecute-Delete.ps1
@@ -0,0 +1,27 @@
+﻿param(
+    [Parameter(Mandatory=$true)]
+    [string]$UniqueName,
+    [Parameter(Mandatory=$true)]
+    [string]$OrgName,
+    [Parameter(Mandatory=$true)]
+    [string]$Region,
+    [Parameter(Mandatory=$true)]
+    [string]$UserName,
+    [Parameter(Mandatory=$true)]
+    [string]$Password
+)
+
+Write-Verbose "Username: $UserName"
+Write-Verbose "Password: $Password"
+
+$connParams = @{
+    OrganizationName = $UniqueName 
+    ServerUrl = "https://$OrgName.$Region.dynamics.com"
+    Credential = [PSCredential]::new($UserName, ($Password | ConvertTo-SecureString -AsPlainText -Force))
+}
+
+$connParams
+
+$connParams.Credential
+
+#.\Helpers\Execute-DeleteDataFromJson.ps1 -CrmConnectionParameters $connParams -Verbose -RecordDirectory /deletes/
